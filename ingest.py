8e8c9c8d0e5d04ac4121dfdcc2fb6819608bee33
Tarun <pandeytarun08082003@gmail.com>
2023-09-20 18:47:02 +0530

    Added pdf loader

--- ingest.py
+++ ingest.py
@@ -15,14 +15,19 @@ DB_FAISS_PATH = "vectorstores/db_faiss"
 # Create Vector Database
 
 def create_vector_db():
-    loader = DirectoryLoader(DATA_PATH,glob='*.csv',loader_cls=CSVLoader)
+    loader = DirectoryLoader(DATA_PATH,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+
     documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
+                                                   chunk_overlap=50)
     texts = text_splitter.split_documents(documents)
 
-    embeddings = HuggingFaceBgeEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2',model_kwargs = {'device':'cpu'})
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'})
 
-    db = FAISS.from_documents(texts,embeddings)
+    db = FAISS.from_documents(texts, embeddings)
     db.save_local(DB_FAISS_PATH)
 
 if __name__ == '__main__':
