7533b7b58d246de15610eb4221891df710bc90d9
Shreya Sanap <sanapshreya2003@gmail.com>
2023-09-25 23:16:11 +0530

    added dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./dashboard.css" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Open+Sans:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <div class="profile-dashboard">
+        <div class="profile-dashboard-child"></div>
+        <div class="frame-parent">
+            <div class="black-1-parent">
+            <img class="black-1-icon" alt="" src="/career-counselling/AI-Career-Counselling/images/user.png" />
+
+                <b class="shreya-sanap">Tarun Pandey</b>
+            </div>
+            <div class="student-wrapper">
+                <div class="student">Student</div>
+            </div>
+        </div>
+
+        <div class="profile-details-">Profile Details :-</div>
+        <b class="test-taken">Test Taken</b>
+        <b class="time-spent">TIme Spent :</b>
+        <b class="marks-scored">Marks Scored</b>
+        <b class="reattempt-test">Reattempt test</b>
+        <b class="take-help-from">Take help from your AI</b>
+        <b class="attempts-taken">Attempts taken</b>
+        <div class="profile-dashboard-item"></div>
+        <div class="profile-dashboard-inner"></div>
+        <div class="aptitude-test-parent">
+            <b class="shreya-sanap">Aptitude test</b>
+            <div class="wednesday">Wednesday</div>
+        </div>
+        <div class="wrapper">
+            <div class="div">45/50</div>
+        </div>
+        <div class="container">
+            <div class="div">3</div>
+        </div>
+        <div class="rectangle-div"></div>
+        <div class="profile-dashboard-child1"></div>
+        <b class="go-to-test">Go to test</b>
+        <b class="go-to-ai">Go to Ai</b>
+        <div class="profile-dashboard-child2"></div>
+        <div class="mon-parent">
+            <div class="mon">Mon</div>
+            <div class="mon">Tue</div>
+            <div class="mon">Wed</div>
+            <div class="mon">Thurs</div>
+            <div class="mon">Fri</div>
+            <div class="mon">Sat</div>
+            <div class="mon">Sun</div>
+        </div>
+        <div class="group-parent">
+            <div class="rectangle-parent">
+                <div class="group-child"></div>
+                <div class="group-item"></div>
+            </div>
+            <div class="rectangle-parent">
+                <div class="group-child"></div>
+                <div class="group-child1"></div>
+            </div>
+            <div class="rectangle-container">
+                <div class="group-child2"></div>
+                <div class="group-child3"></div>
+            </div>
+            <div class="rectangle-parent">
+                <div class="group-child"></div>
+                <div class="group-child5"></div>
+            </div>
+            <div class="rectangle-parent">
+                <div class="group-child"></div>
+                <div class="group-child7"></div>
+            </div>
+            <div class="rectangle-parent">
+                <div class="group-child"></div>
+                <div class="group-child9"></div>
+            </div>
+            <div class="rectangle-parent">
+                <div class="group-child"></div>
+                <div class="group-child7"></div>
+            </div>
+        </div>
+        <div class="class-9th-container">
+            <p class="class-9th">Class : 9th</p>
+            <p class="class-9th">&nbsp;</p>
+            <p class="class-9th">
+                Interests : Playing Badminton,football,collecting coins
+            </p>
+            <p class="class-9th">&nbsp;</p>
+            <p class="class-9th">Major field interests :</p>
+            <ul class="biology-chemistry">
+                <li class="biology">Biology</li>
+                <li class="biology">Chemistry</li>
+            </ul>
+            <p class="class-9th">&nbsp;</p>
+        </div>
+
+
+        <div class="profile-dashboard-child3"></div>
+        <b class="logout">Logout</b>
+    </div>
+</body>
+
+</html>
